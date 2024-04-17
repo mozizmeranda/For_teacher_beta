f663f036834d7138dab378182dbf4723f3c8599a
mozizmeranda <112464452+mozizmeranda@users.noreply.github.com>
2024-04-17 14:12:52 +0500

    proxy added bug fix

--- loader.py
+++ loader.py
@@ -6,6 +6,6 @@ from data import config
 
 PROXY_URL = "http://proxy.server:3128"
 
-bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML, proxy=)
+bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML, proxy=PROXY_URL)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)

b9f33ab1425689b07e587973c7ea6fe5e85d1ff9
mozizmeranda <112464452+mozizmeranda@users.noreply.github.com>
2024-04-25 00:31:55 +0500

    fix

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ from aiogram import types
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 from data import config
+from aiohttp import web
 import asyncio
 
 async def on_startup(dispatcher):
-    await bot.set_webhook(config.WEBHOOK_URL)
+    await dp.bot.set_webhook(config.WEBHOOK_URL)
     await set_default_commands(dispatcher)
     try:
         db.main_db()
@@ -17,13 +18,24 @@ async def on_startup(dispatcher):
     # Notify about startup
     await on_startup_notify(dispatcher)
 
+
+async def process_telegram_update(update):
+    await dp.process_update(update)
+
+
+async def handle(request):
+    if request.match_info.get('token') == config.BOT_TOKEN:
+        data = await request.json()
+        update = types.Update(**data)
+        await dp.process_update(update)
+        return web.Response(text="OK")
+    else:
+        return web.Response(text="Invalid token")
+
 async def on_shutdown(dp):
     await dp.bot.delete_webhook()
 
-async def run_bot():
-    await executor.start_webhook(dispatcher=dp, webhook_path='', skip_updates=True, on_startup=on_startup,
-                                  on_shutdown=on_shutdown, host=config.WEBAPP_HOST, port=config.WEBAPP_PORT)
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run_bot())
+    executor.start_webhook(dispatcher=dp, webhook_path='', skip_updates=True, on_startup=on_startup,
+                                 on_shutdown=on_shutdown, host=config.WEBAPP_HOST, port=config.WEBAPP_PORT)

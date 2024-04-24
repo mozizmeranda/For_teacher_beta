df857438f90a9062272aaf2f561f0fde5ca1d29d
mozizmeranda <112464452+mozizmeranda@users.noreply.github.com>
2024-04-24 21:55:09 +0500

    adding webhook

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from aiogram.utils import executor
 from loader import dp, db
 import middlewares, filters, handlers
+from aiogram import types
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
-
+from data import config
 
 async def on_startup(dispatcher):
-    # Устанавливаем дефолтные команды
+    await dp.bot.set_webhook(config.WEBHOOK_URL, drop_pending_updates=True)
     await set_default_commands(dispatcher)
     try:
         db.main_db()
@@ -17,6 +18,41 @@ async def on_startup(dispatcher):
     await on_startup_notify(dispatcher)
 
 
+async def process_telegram_update(update):
+    await dp.process_update(update)
+
+
+async def handle(request):
+    if request.match_info.get('token') == config.BOT_TOKEN:
+        data = await request.json()
+        update = types.Update(**data)
+        await dp.process_update(update)
+    else:
+        print("Doesn't work")
+
+async def on_startup_handler():
+    await on_startup(dp)
+
+
+async def on_shutdown(dispatcher):
+    await dp.bot.delete_webhook()
+
+
+# async def on_startup(dispatcher):
+#     # Устанавливаем дефолтные команды
+#     await set_default_commands(dispatcher)
+#     try:
+#         db.main_db()
+#     except Exception as e:
+#         print(e)
+#
+#     # Уведомляет про запуск
+#     await on_startup_notify(dispatcher)
+
+
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
+    if __name__ == '__main__':
+        executor.start_webhook(dispatcher=dp, webhook_path=config.WEBHOOK_PATH, on_startup=on_startup, on_shutdown=on_shutdown,
+                               host=config.WEBAPP_HOST, port=config.WEBAPP_PORT)
+    # executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
 
--- data/config.py
+++ data/config.py
@@ -1,4 +1,5 @@
 from environs import Env
+import os
 
 # Теперь используем вместо библиотеки python-dotenv библиотеку environs
 env = Env()
@@ -8,6 +9,10 @@ BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа s
 ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
 channels = [-1001944359018]
 IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
+WEBAPP_HOST = "0.0.0.0"
+WEBAPP_PORT = int(os.environ.get('PORT'))
+WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
+WEBHOOK_URL = f'https:/forteacherbot-96867b4c4706.herokuapp.com/webhook/{BOT_TOKEN}'
 
 
 banned_users = [5928962311]

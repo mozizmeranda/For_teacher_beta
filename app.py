49b1860fff20be7f993fcf9717e6e13f5232676d
mozizmeranda <112464452+mozizmeranda@users.noreply.github.com>
2024-04-24 23:43:55 +0500

    fix

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from aiogram.utils import executor
-from loader import dp, db
+from loader import dp, db, bot
 import middlewares, filters, handlers
 from aiogram import types
 from utils.notify_admins import on_startup_notify
@@ -7,8 +7,9 @@ from utils.set_bot_commands import set_default_commands
 from data import config
 import asyncio
 
+
 async def on_startup(dispatcher):
-    await dp.bot.set_webhook(config.WEBHOOK_URL)
+    await bot.set_webhook(config.WEBHOOK_URL)
     await set_default_commands(dispatcher)
     try:
         db.main_db()
@@ -19,18 +20,6 @@ async def on_startup(dispatcher):
     await on_startup_notify(dispatcher)
 
 
-async def process_telegram_update(update):
-    await dp.process_update(update)
-
-
-async def handle(request):
-    if request.match_info.get('token') == config.BOT_TOKEN:
-        data = await request.json()
-        update = types.Update(**data)
-        await dp.process_update(update)
-    else:
-        print("Doesn't work")
-
 async def on_startup_handler():
     await on_startup(dp)
 
@@ -39,22 +28,7 @@ async def on_shutdown(dispatcher):
     await dp.bot.delete_webhook()
 
 
-# async def on_startup(dispatcher):
-#     # Устанавливаем дефолтные команды
-#     await set_default_commands(dispatcher)
-#     try:
-#         db.main_db()
-#     except Exception as e:
-#         print(e)
-#
-#     # Уведомляет про запуск
-#     await on_startup_notify(dispatcher)
 
-executor.start_webhook(dispatcher=dp, webhook_path='', skip_updates=True, on_startup=on_startup,
+if __name__ == '__main__':
+    executor.start_webhook(dispatcher=dp, webhook_path='', skip_updates=True, on_startup=on_startup,
                            on_shutdown=on_shutdown, host=config.WEBAPP_HOST, port=config.WEBAPP_PORT)
-
-
-# if __name__ == '__main__':
-#     asyncio.run(main())
-    # executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
-

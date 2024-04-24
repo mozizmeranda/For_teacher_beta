fe22983d89101c48a5aa81a958d1883c47b7aa3b
mozizmeranda <112464452+mozizmeranda@users.noreply.github.com>
2024-04-24 22:19:50 +0500

    fix bug

--- app.py
+++ app.py
@@ -51,8 +51,7 @@ async def on_shutdown(dispatcher):
 
 
 if __name__ == '__main__':
-    if __name__ == '__main__':
-        executor.start_webhook(dispatcher=dp, webhook_path=config.WEBHOOK_PATH, on_startup=on_startup, on_shutdown=on_shutdown,
-                               host=config.WEBAPP_HOST, port=config.WEBAPP_PORT)
+    executor.start_webhook(dispatcher=dp, webhook_path=config.WEBHOOK_PATH, on_startup=on_startup, on_shutdown=on_shutdown,
+                            host=config.WEBAPP_HOST, port=config.WEBAPP_PORT)
     # executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
 

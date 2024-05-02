65d3f4ad0406eb5a06c9e0050bc0f283fab86fd5
mozizmeranda <112464452+mozizmeranda@users.noreply.github.com>
2024-05-02 16:20:58 +0500

    fix

--- app.py
+++ app.py
@@ -5,17 +5,6 @@ from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 from data import config
 
-# async def on_startup(dispatcher):
-#     await set_default_commands(dispatcher)
-#     try:
-#         await db.main_db()
-#     except Exception as e:
-#         print(e)
-#
-#     # db.delete_users()
-#     await on_startup_notify(dispatcher)
-
-
 async def on_startup(dispatcher):
     await dp.bot.set_webhook(config.WEBHOOK_URL, drop_pending_updates=True)
     await set_default_commands(dispatcher)
@@ -38,5 +27,3 @@ async def on_shutdown(dispatcher):
 if __name__ == "__main__":
     executor.start_webhook(dispatcher=dp, webhook_path=config.WEBHOOK_PATH, on_startup=on_startup,
                            on_shutdown=on_shutdown, host=config.WEBAPP_HOST, port=config.WEBAPP_PORT)
-    # executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
-

86172706d1694ee2c6bbe2d15e0629d8f845e315
mozizmeranda <112464452+mozizmeranda@users.noreply.github.com>
2024-05-02 16:41:05 +0500

    fix

--- data/config.py
+++ data/config.py
@@ -1,5 +1,5 @@
 from environs import Env
-# import os
+import os
 
 # Теперь используем вместо библиотеки python-dotenv библиотеку environs
 env = Env()
@@ -9,8 +9,8 @@ BOT_TOKEN = "7196106888:AAG1v1ncrexwL2pgWO7PbPDrPqeAj-YmZdE"  # Забираем
 ADMINS = [5928962311]  # Тут у нас будет список из админов
 channels = [-1001944359018]
 IP = "52.91.95.42"  # Тоже str, но для айпи адреса хоста
-WEBAPP_HOST = f"https://{IP}"
-WEBAPP_PORT = 8443
+WEBAPP_HOST = f"0.0.0.0"
+WEBAPP_PORT = 3001
 WEBHOOK_PATH = f'/bot/{BOT_TOKEN}'
 WEBHOOK_URL = f'{WEBAPP_HOST}:{WEBAPP_PORT}{WEBHOOK_PATH}'
 
